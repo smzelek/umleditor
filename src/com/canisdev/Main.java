0092176584bb813110da530ef5af16c912c3735a
Steven M Zelek <smzelek@gmail.com>
2017-02-03 17:06:06 -0500

    Added menu and CSS styling

--- src/com/canisdev/Main.java
+++ src/com/canisdev/Main.java
@@ -1,19 +1,13 @@
 package com.canisdev;
 
-
 import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -22,59 +16,31 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-
     @Override
     public void start(Stage primaryStage){
-        primaryStage.setTitle("Authentication");
+        primaryStage.setTitle("CANIS UML Editor 1.0");
 
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        //grid.setPadding(new Insets(25, 25, 25, 25));
+        VBox vBox = new VBox();
 
-        Text sceneTitle = new Text("Welcome");
-        sceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-        grid.add(sceneTitle, 0, 0, 2, 1);
+        MenuBar menuBar = new MenuBar();
 
-        Label userName = new Label("User Name:");
-        grid.add(userName, 0, 1);
+        Menu menuFile = new Menu("File");
+        //new, open, save, settings, exit
+        MenuItem newFile = new MenuItem("New");
 
-        TextField userTextField = new TextField();
-        grid.add(userTextField, 1,1);
+        menuFile.getItems().addAll(newFile);
 
-        Label pw = new Label("Password:");
-        grid.add(pw, 0, 2);
+        Menu menuEdit = new Menu("Edit");
+        Menu menuView = new Menu("View");
 
-        PasswordField pwBox = new PasswordField();
-        grid.add(pwBox, 1, 2);
+        menuBar.getMenus().addAll(menuFile, menuEdit, menuView);
+        vBox.getChildren().addAll(menuBar);
 
-        Button btn = new Button("Sign in");
-
-
-        final Text actiontarget = new Text();
-        grid.add(actiontarget, 1, 6);
-
-
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                actiontarget.setFill(Color.FIREBRICK);
-                actiontarget.setText("Sign in button pressed");
-            }
-        });
-        
-        HBox hbBtn = new HBox(10);
-        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-        hbBtn.getChildren().add(btn);
-        grid.add(hbBtn, 1, 4);
-
-        //grid.setGridLinesVisible(true);
-        Scene scene = new Scene(grid, 300, 275);
+        Scene scene = new Scene (vBox, 400,450);
 
         primaryStage.setScene(scene);
-        primaryStage.setResizable(false);
-        scene.getStylesheets().add(Main.class.getResource("Login.css").toExternalForm());
+        //primaryStage.setResizable(false);
+        scene.getStylesheets().add(Main.class.getResource("Style1.css").toExternalForm());
         primaryStage.show();
     }
     public static void main(String[] args) {
